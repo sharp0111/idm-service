323bce90f4fff101f0da8d1a6ac21ebf7201299e
Sergey Nam <shaqk702@gmail.com>
2022-01-04 22:07:39 -0800

    Add primitive type hints

--- api/route/__init__.py
+++ api/route/__init__.py
@@ -5,17 +5,18 @@ import requests
 class VendorService:
     """ VendorService """
 
-    def __init__(self, vendor_url, locale, vendor_api_host, vendor_api_key):
+    def __init__(self, vendor_url: str, locale: str, vendor_api_host: str, vendor_api_key: str):
         self.vendor_url = vendor_url
         self.locale = locale
         self.x_rapid_api_host = vendor_api_host
         self.x_rapid_api_key = vendor_api_key
 
-    def search(self, term):
+    def search(self: str, term: str):
         """Search"""
         params = {"term": term, "locale": self.locale}
         headers = {"X-RapidAPI-Host": self.x_rapid_api_host,
                    "X-RapidAPI-Key": self.x_rapid_api_key}
         res = requests.get(self.vendor_url + "/search",
                            headers=headers, params=params)
+        print(res)
         return res.json()
