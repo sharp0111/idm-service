4f741a3a63048a179f0aff026e59b7fb38cab584
Sergey Nam <shaqk702@gmail.com>
2022-01-05 18:39:56 -0800

    Remove root URL

--- app.py
+++ app.py
@@ -10,12 +10,6 @@ vendorService = VendorService(
     Config.VENDOR_URL, Config.LOCALE, Config.X_RAPID_API_HOST, Config.X_RAPID_API_KEY)
 
 
-@app.route("/")
-def hello_world():
-    """Hello World"""
-    return "<p>Hello, World!</p>"
-
-
 @app.route("/search", methods=["GET"])
 def search():
     """Search"""
